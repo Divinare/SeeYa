12ad2620dc779c82741ac7f795e5d51dbbc36d35
Pete <pmmakinen@hotmail.com>
2015-07-10 17:13:01 +0300

    old controllers returned and server.js refactored

---
+++ backend/controllers/EventController.js
@@ -0,0 +1,66 @@
+ module.exports = {
+
+ 	findOne: function (req, res) {
+ 		var eventId = req.params.id;
+ 		Event.findOne({
+ 			where: { id: eventId }
+ 		}).populate('address')
+ 		.then(function (event) {
+ 			console.log(event);
+ 			res.send(event);
+ 		});
+ 	},
+
+ 	findAll: function (req, res) {
+
+ 		Event.find().then(function (events) {
+ 			res.send(events);
+ 		});
+
+ 	},
+ 	create: function (req, res) {
+ 		var eventToAdd = req.body;
+ 		console.log(eventToAdd.address)
+ 	var address
+ 	Address.create({
+ 		streetAddress: eventToAdd.address.streetAddress
+ 	}).then(function(address){
+
+ 		Event.create({
+ 			name: eventToAdd.name,
+ 			description: eventToAdd.description,
+ 			date: eventToAdd.date,
+ 			lat: eventToAdd.lat,
+ 			lon: eventToAdd.lon,
+ 			requiresRegistration: eventToAdd.requiresRegistration,
+ 			address: address
+ 		}).then(function(model) {
+ 			console.log(model.name + ' on lisätty tietokantaan onnistuneesti!');
+	      // Palauta vastauksena lisätty aihealue
+	      res.send(model);
+	  });
+ 	});
+
+ },
+
+ update: function (req, res) {
+ 	var controlId = req.params.id;
+ 	var requiredProps = ['name', 'description',]
+ 	Event.findOne({
+ 		where: { id: controlId}
+ 	}).then(function( event) {
+ 		event.name = req.body['name'];
+ 		event.description = req.body['description']
+ 		event.date = req.body['date']
+ 		event.lat = req.body['lat']
+ 		event.lon = req.body['lon']
+ 		event.requiresRegistration = req.body['requiresRegistration']
+ 		event.save(function(err) {
+ 			if (err) {
+ 				return res.send(err);
+ 			}
+ 			res.send(200);
+ 		});
+ 	});
+ }
+};
