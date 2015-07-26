3c2d41247b322bfd6da22ed0166012cbb0f9a4a1
Joe <niemijoe@hotmail.com>
2015-07-26 18:48:25 +0300

    migrations

--- frontend/main.js
+++ frontend/main.js
@@ -27,6 +27,7 @@ var Main = React.createClass({
   },
   componentWillMount: function() {
        this.state.eventList.push("jee");
+       
   },
 
   componentDidMount: function() {
@@ -36,9 +37,6 @@ var Main = React.createClass({
 
   render: function() {
     
- // <Header />
-// <RouteHandler eventList={this.state.eventList} />
-
     return (
           <div>
             <Header />
@@ -60,84 +58,18 @@ var EventListsWrapper = React.createClass({
   }
 });
 
-
-
-/*
-
-var routes = (
-    <Route handler={Main}>
-      <DefaultRoute name="home" handler={EventListsWrapper} />
-      <Route name="about" handler={About} />
-      <Route name="eventForm" handler={EventForm} />
-      <Route name="/events/:id" handler={EventPage} />
-      <Route path="error" handler={NoMatch} />
-      <NotFoundRoute handler={NoMatch}/>
-    </Route>
-);
-*/
-
-//       <DefaultRoute name="home" handler={EventListsWrapper} />
-// <Route name='events' path='/events/:id' handler={EventPage} />
-
-/*
-React.render((
-  <Router history={BrowserHistory}>
-    <Route path="/" component={Main}>
-      <Route path="home" component={EventListsWrapper}/>
-      <Route path="about" component={About}/>
-      <Route name="eventPage" path="events/:someparam" handler={EventPage} />
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.getElementById('container'));
-*/
-
-
-
-
 var routes = (
   <Route handler={Main} path="/">
-    <DefaultRoute name="home" path="/" handler={EventListsWrapper} />
+    <Route name="home" path="/" handler={EventListsWrapper} />
     <Route name="eventPage" path="events/:someparam" handler={EventPage} />
     <Route name="eventForm" handler={EventForm} />
     <Route name="about" handler={About} />
+    <Route path="*" handler={NoMatch}/>
   </Route>
 );
-
-// Router.HistoryLocation, 
 
 $(document).ready(function () {
   Router.run(routes, Router.HistoryLocation, function (Handler) {
     React.render(<Handler/>, document.body);
   });
 });
-
-
-/*
-      <Route path="users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
-      </Route>
-
-*/
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
-/*
-React.render(
-  <CommentBox />,
-  document.getElementById('container')
-);
-*/
-/*
-$(document).ready(function () {
-  React.render(<CommentBox />, document.body);
-});
-
-*/
