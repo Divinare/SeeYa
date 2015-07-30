f38b4ab331564261ba9e90f334004374552e92d3
Pete <pmmakinen@hotmail.com>
2015-07-30 21:51:52 +0300

    duplicate addresses not created anymore

--- backend/controllers/EventController.js
+++ backend/controllers/EventController.js
@@ -5,13 +5,13 @@ var Sequelize = require('sequelize')
 module.exports = {
 
   findOne: function (req, res) {
-     var eventId = req.params.id;
-     models.Event.findOne({
-        where: { id: eventId }
-    })
-     .then(function (event) {
-        res.send(event);
-    });
+   var eventId = req.params.id;
+   models.Event.findOne({
+    where: { id: eventId }
+  })
+   .then(function (event) {
+    res.send(event);
+  });
  },
 
   /*
@@ -31,7 +31,7 @@ module.exports = {
   findAll: function (req, res) {
     models.Event.findAll({
       include: [ models.Address ]
-  }).then(function (events) {
+    }).then(function (events) {
         /*
           if (error) {
             res.status(500).send(error);
@@ -39,60 +39,63 @@ module.exports = {
           }
           */
           res.send(events);
-      });
-},
+        });
+  },
 
-create: function (req, res) {
- var eventToAdd = req.body;
- console.log(eventToAdd.address)
+  create: function (req, res) {
+   var eventToAdd = req.body;
+   console.log(eventToAdd.address)
 
- models.Address.create({
+   models.Address.findOrCreate({where: {
     streetAddress: eventToAdd.address.streetAddress,
     country: eventToAdd.address.country,
     zipCode: eventToAdd.address.zipCode
-}).then(function(address){
-      console.log("--------------------------------------------------------")
-      console.log("address id: " + address.id)
-      console.log(address)
+  }
 
-      console.log("------------------------------------------------------------")
-      models.Event.create({
-        name: eventToAdd.name,
-        description: eventToAdd.description,
-        lat: eventToAdd.lat,
-        lon: eventToAdd.lon,
-        timestamp: eventToAdd.timestamp,
+}).spread(function(address, created){
+  console.log("--------------------------------------------------------")
+  console.log("address id: " + address.id)
+  console.log(address)
+  console.log("created: " + created)
+
+  console.log("------------------------------------------------------------")
+  models.Event.create({
+    name: eventToAdd.name,
+    description: eventToAdd.description,
+    lat: eventToAdd.lat,
+    lon: eventToAdd.lon,
+    timestamp: eventToAdd.timestamp,
           //requiresRegistration = eventToAdd.requiresRegistration,
-      }).then(function(event) {
-        event.setAddress(address)
-        console.log(event.name + ' created successfully');
-        res.send(event);
-});
+        }).then(function(event) {
+          event.setAddress(address)
+          console.log(event.name + ' created successfully');
+          res.send(event);
+        });
 
-});
+      });
 
 
-   
+
 },
 
 update: function (req, res) {
   var controlId = req.params.id;
   var requiredProps = ['name', 'description',]
   models.Event.findOne({
-     where: { id: controlId}
+   where: { id: controlId}
  }).then(function( event) {
-     event.name = req.body['name'];
-     event.description = req.body['description']
-     event.date = req.body['date']
-     event.lat = req.body['lat']
-     event.lon = req.body['lon']
+   event.name = req.body['name'];
+   event.description = req.body['description']
+   event.date = req.body['date']
+   event.lat = req.body['lat']
+   event.lon = req.body['lon']
  	//	event.requiresRegistration = req.body['requiresRegistration']
-     event.save(function(err) {
-        if (err) {
-           return res.send(err);
-       }
-       res.send(200);
-   });
+   event.save(function(err) {
+    if (err) {
+     return res.send(err);
+   }
+   res.send(200);
+ });
  });
 }
 
--- backend/models/address.js
+++ backend/models/address.js
@@ -3,8 +3,8 @@
 module.exports = function(sequelize, DataTypes) {
 	var Address = sequelize.define("Address", {
     streetAddress: { type: DataTypes.STRING, allowNull: false, validate: { len: { args: [3,30], msg: "Name must be at least 3-30 characters long"} } },
-
     country: { type: DataTypes.STRING, allowNull: false },
+    city: { type: DataTypes.STRING, allowNull: true},
     zipCode: { type: DataTypes.STRING, allowNull: false }
     
 
