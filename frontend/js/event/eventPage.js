97f83a13a0b946dce2da7868844cb574ac0550ea
Pete <pmmakinen@hotmail.com>
2015-08-02 20:30:28 +0300

    events can now be attended through the ui

--- frontend/js/event/eventPage.js
+++ frontend/js/event/eventPage.js
@@ -33,20 +33,59 @@ var EventPage = React.createClass({
 			}
 		}.bind(this));*/
 
-		$.ajax({ 
-			type: 'GET', 
-			url: url,
-			dataType: 'json',
-			success: function (data) { 
-				if(that.isMounted()){
-					that.setState({
-						event: data
-					})
-				}
-			}
-	});
+$.ajax({ 
+	type: 'GET', 
+	url: url,
+	dataType: 'json',
+	success: function (data) { 
+		if(that.isMounted()){
+			that.setState({
+				event: data
+			})
+		}
+	}
+});
 
-	},
+},
+
+handleSubmit: function(e) {
+	var that = this;
+	var event = this.state.event;
+	console.log("name: " + this.state.event.name)
+	e.preventDefault();
+	var data = {
+		attendeeName: this.state.attendeeName,
+		email: this.state.email,
+		comment: this.state.comment,
+		event: event
+	}
+	
+	$.ajax({
+	    type: 'POST',
+	    dataType: 'json',
+	    url: URL.attendance,
+	    data: JSON.stringify(data),
+	    contentType: "application/json; charset=utf-8",
+	    //contentType: 'application/x-www-form-urlencoded',
+	    success: function(){
+	       // that.transitionTo('home');
+	    },
+	    error: function( jqXhr, textStatus, errorThrown ){
+	        console.log( errorThrown );
+	    }
+	})
+
+},
+
+handleChange: function(key) {
+	console.log("handle change")
+	console.log(this.state.attendeeName)
+	return function (e) {
+		var state = {};
+		state[key] = e.target.value;
+		this.setState(state);
+	}.bind(this);
+},
 
 render: function(){
 		//console.log("result: " + utils.urlTokens())
@@ -54,17 +93,9 @@ render: function(){
 		var event = this.state.event
 		var date = Moment.unix(event.timestamp/1000).format("ddd DD.MM.YYYY");
 		var time = Moment.unix(event.timestamp/1000).format("HH:mm")
-		console.log("timestamp: " + event.timestamp)
-		console.log(event)
-		console.log("milliseconds now: " + new Date().getTime())
-
-		console.log("event name: " + event.name)
-		console.log(event)
-		console.log(event.Address)
 		//console.log("type: " + typeof event.address)
 
 		var address;
-		
 
 		if(typeof event.Address === 'undefined'){
 			address = <div></div>
@@ -84,7 +115,7 @@ render: function(){
 				addressStr += event.Address.country
 			}
 			addressStr = Underscore.trim(addressStr, ", ")
-		
+
 			address = <div><b>Address:</b> {addressStr}</div>
 
 			console.log('-----------------')
@@ -102,12 +133,23 @@ render: function(){
 					{address}
 				</div>
 				<div id='rightPane' className='col-xs-12 col-md-6'>
-					<h1>Attend to the event</h1>
+					<h1>Attend the event</h1>
 					<form className='form' role='form' onSubmit={ this.handleSubmit }>
+						<div className='form-group'>
+							<input type='text' value={this.state.attendeeName} onChange={this.handleChange('attendeeName')} className='form-control' id='attendeeName' placeholder='Your name'/>
+						</div>
+						<div className='form-group'>
+							<input type='text' value={this.state.email} onChange={this.handleChange('email')} className='form-control' id='email' placeholder='Email address'/>
+						</div>
+						<div className='form-group'>
+							<textArea type='text' value={this.state.description} onChange={this.handleChange('comment')} className='form-control' id='comment' placeholder='Comment'/>
+						</div>
+						<div className="form-group">
+				            <button className="btn btn-default" type="submit">Attend</button>
+					    </div>
+
 					</form>
 				</div>
-
-
 			</div>
 
 
