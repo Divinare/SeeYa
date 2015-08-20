b73b38c3f31f43fa435286fcb07ba67d69517cbf
Pete <pmmakinen@hotmail.com>
2015-08-20 21:38:46 +0300

    the app uses postgres now

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,8 +5,19 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+//var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+  //var sequelize = new Sequelize('postgres://null:localhost:5432/dbname');
 
 fs
   .readdirSync(__dirname)
