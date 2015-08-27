6162b0f291909dfd2c10e9c26be3eed0dbe74721
Joe <niemijoe@hotmail.com>
2015-08-27 16:47:31 +0300

    config

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,7 +7,19 @@ var env       = "production"; //process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../../config/config.json')[env];
 //var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
-var sequelize = new Sequelize(config.database, config.username, config.password, {
+var sequelize = null;
+
+if (env == "production") {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.PRODUCTION_DB_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: config.dialect,
 
