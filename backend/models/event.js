df4a01909170277b654f61d2eefbcea2b41b8a88
Pete <pmmakinen@hotmail.com>
2015-08-27 13:31:25 +0300

    models changes

--- backend/models/event.js
+++ backend/models/event.js
@@ -3,11 +3,12 @@
 module.exports = function(sequelize, DataTypes) {
 	var Event = sequelize.define("Event", {
     name: { type: DataTypes.STRING, allowNull: false, validate: { len: { args: [3,30], msg: "Name must be at least 3-30 characters long"} } },
-    description: { type: DataTypes.STRING, allowNull: true },
+    description: { type: DataTypes.STRING, allowNull: true , validate: { len: { args: [0,500]}}},
     lon: { type: DataTypes.FLOAT, allowNull: false },
     lat: { type: DataTypes.FLOAT, allowNull: false },
     requiresRegistration: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
-    timestamp: {type : DataTypes.BIGINT, allowNull: false}
+    timestamp: {type : DataTypes.BIGINT, allowNull: false},
+    maxAttendees: {type : DataTypes.INTEGER, allowNull: true}
   }, {
         timestamps: true,
         classMethods: {
