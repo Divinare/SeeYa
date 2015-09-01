bca6719b09cc89e79492c6b3ebff3519380182d9
Joe <niemijoe@hotmail.com>
2015-09-01 22:09:18 +0300

    config to heroku

--- frontend/configs/environment.js
+++ frontend/configs/environment.js
@@ -2,9 +2,9 @@ module.exports = {
 	environment: 'development',
 	base: function() {
 		if(this.environment == 'development') {
-			return 'http://localhost:1337';
+			return '//localhost:1337';
 		} else if(this.environment == 'production') {
-			return 'http://event-meetup.herokuapp.com';
+			return '//event-meetup.herokuapp.com';
 		}
 	}
 
