54d3fcc44972ca7918995b70ca9ec5c910fc962d
Pete <pmmakinen@hotmail.com>
2016-04-30 18:57:42 +0300

    fb login to production

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1215884721762644', // your App ID
         'clientSecret'  : 'a9b5511ec7bfb3e7af064ed5fe4e3dd6', // your App Secret
-        'callbackURL'   : 'http://localhost:1337/api/auth/facebook/callback'
+        'callbackURL'   : 'https://seeyaevents.com/api/auth/facebook/callback'
     },
 };
