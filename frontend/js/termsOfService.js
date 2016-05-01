06cc90b30c5d6848b6f75baef2537e75b2423187
Pete <pmmakinen@hotmail.com>
2016-05-01 19:14:23 +0300

    added terms of service

--- frontend/js/termsOfService.js
+++ frontend/js/termsOfService.js
@@ -40,13 +40,19 @@ const TermsOfService = React.createClass({
 
                 <p>SeeYa is a community for creating and joining events nearby. Discover new events, gain new experiences with other people and have fun.</p>
                 <p>Simple and easy way to explore events near you or far away. SeeYa was developed in 2016 by a small group of friends in Helsinki, Finland.
-                <em>This text is emphasized.</em></p>
+                </p>
 
               <dl>
-                <dt>1. Key Terms</dt>
-                <dd> SeeYa is a community for creating and joining events nearby. Discover new events, gain new experiences with other people and have fun. </dd>
-                <dt>2. Account Registeration</dt>
-                <dd> SeeYa is a community for creating and joining events nearby. Discover new events, gain new experiences with other people and have fun. </dd>
+                <dt>1. Illegal content</dt>
+                    <dd>Do not post any illegal content or content that others may consider offensive.</dd>
+                <dt>2. Inappropriate content</dt>
+                    <dd>Inappropriate events may get deleted without notice or explanation</dd>
+                <dt>3. Cookies</dt>
+                    <dd>SeeYa uses cookies. By using this website and agreeing to these terms and conditions, you consent to SeeYa’s use of cookies.</dd>
+                <dt>4. Use common sense</dt>
+                    <dd>Use common sense when meeting people you don’t know. SeeYa or its developers take no responsibility in anything that happens in the events organized by using SeeYa.</dd>
+                <dt>5. Follow the rules</dt>
+                    <dd>Breaking any of these rules or other malicious actions towards the website or its users may result in the user account being terminated without notice or explanation.</dd>
               </dl>    
 
 
