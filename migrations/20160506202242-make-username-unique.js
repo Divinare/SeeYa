cde2abc88e5b1b329e15b5814ad9459bff8c9816
Pete <pmmakinen@hotmail.com>
2016-05-06 20:40:30 +0300

    username is unique now, migration created

--- migrations/20160506202242-make-username-unique.js
+++ migrations/20160506202242-make-username-unique.js
@@ -14,7 +14,7 @@ module.exports = {
         'username',
         {
           type: Sequelize.STRING,
-          allowNull: true,
+          allowNull: false,
           unique:true,
           validate: { len: { args: [3,30]}}
         }
