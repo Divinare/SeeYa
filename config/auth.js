f9d63c3cbff62b0ee5ee08ac9ffc54abd7485635
Joe <niemijoe@hotmail.com>
2016-10-23 18:25:37 +0300

    to heroku

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1215884721762644', // your App ID
         'clientSecret'  : 'a9b5511ec7bfb3e7af064ed5fe4e3dd6', // your App Secret
-        'callbackURL'   : 'https://seeyaevents.com/api/auth/facebook/callback'
+        'callbackURL'   : 'www.seeyaevents.com/api/auth/facebook/callback'
     },
 };
